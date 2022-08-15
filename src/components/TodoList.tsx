91899e0d48d7ca1bf862cb7ef12e068bcb4fc766
alpetrushkin <alpetrushkin1984@mail.ru>
2022-08-15 22:14:50 +0300

    lesson1

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -26,10 +26,6 @@ export const TodoList = (props: TodoListType) => {
                   <li key={el.id}><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
                )
             })}
-
-            {/*<li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>*/}
-            {/*<li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>*/}
-            {/*<li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>*/}
          </ul>
          <div>
             <button>All</button>
